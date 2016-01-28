ec241151cecf30b921c8b15a422c8fc8649a16bc
lebron dong <lazio10000@gmail.com>
2016-01-28 14:02:41 +0800

    1.add mongodb client and save lottery result 2.fix RC code bug

--- index.js
+++ index.js
@@ -4,15 +4,43 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000; 
+// Setup mongodb client
+var mongoClient = require('mongodb').MongoClient; 
+var mongoHost =   process.env.MONGODB_PORT_27017_TCP_ADDR || 'localhost';
+var mongoPort =   process.env.MONGODB_PORT_27017_TCP_PORT || 27017;
+var mongoDatabase =  process.env.MONGODB_INSTANCE_NAME || 'test';
+var mongoUsername =  process.env.MONGODB_USERNAME;
+var mongoPassword =  process.env.MONGODB_PASSWORD;
+var mongoUrl =  "tcp://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort.toString() + "/" + mongoDatabase;
 
-server.listen(port, function () {
-	console.log('Server listening at port %d', port);
+ 
+server.listen(3000, function () {
+	console.log('Server listening at port %d', 3000);
 });
-
+ 
 // Routing
 app.use(express.static(__dirname + '/public')); 
 var usernames = {};
 var numUsers = 0;
+
+var insertData = function(db, winnerList, callback) {  
+  //连接到表  
+  var collection = db.collection('WinnerList');
+  //插入数据 
+  var lotteryDate = new Date();
+  var data = [];
+  winnerList.forEach(function(winner){
+		data.push({"LotteryPeriod": lotteryDate,"Winner": winner});
+  });
+  collection.insert(data, function(err, result) { 
+    if(err)
+    {
+      console.log('Error:'+ err);
+      return;
+    }	 
+    callback(result);
+  });
+}
 
 io.on('connection', function (socket) {
 	var addedUser = false;
@@ -24,7 +52,13 @@ io.on('connection', function (socket) {
 		});
 	});
 	
-	socket.on('lottery', function (data) {   
+	socket.on('lottery', function (data) {
+		mongoClient.connect(url, function(err, db) {
+		  insertData(db, data, function(result) {
+			console.log(result);
+			db.close();
+		  }); 
+		});
 		socket.broadcast.emit('lottery', { 
 			message: data.toString()
 		});
